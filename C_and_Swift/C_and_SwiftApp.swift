e1ec7a3946575e7541e1b707cc9a6656d4632b84
Andrew Trotman <andrew@trotman.com>
2021-06-06 12:45:46 +1200

    Initial Commit

---
+++ C_and_Swift/C_and_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  C_and_SwiftApp.swift
+//  C_and_Swift
+//
+//  Created by Andrew Trotman on 6/06/21.
+//
+
+import SwiftUI
+
+@main
+struct C_and_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C_and_Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  C_and_Swift
+//
+//  Created by Andrew Trotman on 6/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
