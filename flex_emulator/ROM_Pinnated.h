ce0f26e3ec223d3e392df8e17f0a2f741d57f9e3
Andrew Trotman <andrew@trotman.com>
2021-07-10 17:03:00 +1200

    cleaning up

--- flex_emulator/ROM_Pinnated.h
+++
@@ -1,9 +0,0 @@
-/*
-	ROM_PINNATED.H
-	--------------
-	Copyright (c) 2021 Andrew Trotman
-*/
-
-#pragma once
-
-extern const unsigned char eprom_pinnated[];
--- flex_emulator/ROM_TinyBasic.h
+++
@@ -1,8 +0,0 @@
-/*
-	ROM_TINYBASIC.H
-	---------------
-	Copyright (c) 2021 Andrew Trotman
-*/
-
-#pragma once
-extern const unsigned char eprom_TinyBasic[];
