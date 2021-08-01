d53b028c830b89e8a98a21111e2e9cd4dae4bb30
Andrew Trotman <andrew@trotman.com>
2021-08-01 14:38:32 +1200

    script to build system disk

---
+++ extras/disk_build/build.sh
@@ -0,0 +1,37 @@
+new flex.dsk t254,s254
+mount 0 flex.dsk
+label 0 flex
+
+mount 1 original_flex09_AB178.dsk
+copy 1 0
+
+mount 1 flex_srces.dsk
+copy 1 0
+
+mount 1 DIAG6809.DSK
+copy 1 0
+
+mount 1 F77.DSK	
+copy 1 0
+
+mount 1 XBASIC_2.DSK
+copy 1 0
+
+mount 1 RBASIC.DSK
+copy 1 0
+
+mount 1 introl_c/IC1.DSK
+copy 1 0
+
+mount 1 introl_c/IC2.DSK
+copy 1 0
+
+mount 1 introl_c/IC3.DSK
+copy 1 0
+
+mount 1 advgames.dsk
+copy 1 0
+
+mount 1 ADVENT.dsk
+copy 1 0
+
