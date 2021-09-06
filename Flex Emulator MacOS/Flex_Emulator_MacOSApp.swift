8115c345fa3f6fdef9bf4c61fbde037a758ed7c2
Andrew Trotman <andrew@trotman.com>
2021-09-06 19:51:32 +1200

    Got the paste menu working!

--- Flex Emulator MacOS/Flex_Emulator_MacOSApp.swift
+++ Flex Emulator MacOS/Flex_Emulator_MacOSApp.swift
@@ -17,8 +17,12 @@ struct Flex_Emulator_MacOSApp: App
 	{
     @Environment(\.openURL) var openURL
     @StateObject var app_state = AppState.shared
-    @State private var clipboard_has_text = false
-    @State private var acknowledgements_open = false
+    @State var clipboard_has_text = true
+    @State var text_selected = true
+    @State var acknowledgements_open = false
+    @State var disk_0: String = "Flex"
+    @State var active = false
+
 
 	var body: some Scene
 		{
@@ -42,22 +46,7 @@ struct Flex_Emulator_MacOSApp: App
 				{
 				CommandGroup(replacing: .pasteboard)
 					{
-			//		Button("Copy"){}.keyboardShortcut("C").disabled(true)
-					Button("Paste")
-						{
-						if let paste_buffer = NSPasteboard.general.pasteboardItems?.first?.string(forType: .string)
-							{
-							for char in paste_buffer
-								{
-								if let ascii = char.asciiValue
-									{
-									machine_queue_key_press(AppState.shared.machine, CChar(ascii))
-									}
-								}
-							}
-						}
-						.keyboardShortcut("V").disabled(clipboard_has_text)
-			//		Button("Select All"){}.keyboardShortcut("A").disabled(true)
+					cut_copy_paste_menu(can_cut: $text_selected, can_paste: $clipboard_has_text)
 					}
 				CommandGroup(replacing: .undoRedo) {}
 				CommandGroup(replacing: .sidebar) {}
@@ -94,5 +83,32 @@ struct Flex_Emulator_MacOSApp: App
 			{
 			Acknowledgements(state: $acknowledgements_open)
 			}.handlesExternalEvents(matching: Set(arrayLiteral: "Acknowledgements"))
+		}
+	}
+
+struct cut_copy_paste_menu: View
+	{
+	@Binding var can_cut: Bool
+	@Binding var can_paste: Bool
+
+	var body: some View
+		{
+//		Button("Copy"){}.keyboardShortcut("C").disabled(true)
+		Button("Paste")
+			{
+			if let paste_buffer = NSPasteboard.general.pasteboardItems?.first?.string(forType: .string)
+				{
+				for char in paste_buffer
+					{
+					if let ascii = char.asciiValue
+						{
+						machine_queue_key_press(AppState.shared.machine, CChar(ascii))
+						}
+					}
+				}
+			}
+			.disabled(!can_paste)
+			.keyboardShortcut("v")
+//		Button("Select All"){}.keyboardShortcut("A").disabled(true)
 		}
 	}
